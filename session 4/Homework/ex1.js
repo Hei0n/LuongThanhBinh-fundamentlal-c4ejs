6593eede14187178310d4c2892fb3c3cb8e5fc45
Hei0n <52815245+Hei0n@users.noreply.github.com>
2019-07-19 15:21:44 +0700

    Add files via upload

---
+++ session 4/Homework/ex1.js
@@ -0,0 +1,11 @@
+let inventory = {
+    gold : 500,
+    pouch: ['flint','twine','gemstone'],
+    backpack: ['xylophone','dagger','bedroll','bread loaf']
+}
+
+inventory.pocket=['seashell','strange berry','lint'];
+
+delete inventory.backpack[1];
+inventory.gold += 50;
+console.log(inventory);
